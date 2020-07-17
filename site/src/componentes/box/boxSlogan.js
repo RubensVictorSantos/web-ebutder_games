64a5fa6318459c340c4b426bf16d08fbfc0894b7
Rubens Victor <rubensvictorsantos@gmail.com>
2020-07-17 18:48:23 -0300

    criando box index

--- site/src/componentes/box/boxSlogan.js
+++ site/src/componentes/box/boxSlogan.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import imagemLogo from './img/img_logo1.jpg';
-import HeaderIndex, {ImgHeader} from './style'
+import HeaderIndex, {
+    ImgHeader,
+} from './style'
 
 export class ContainerHeader extends Component {
     render() {
 
         return (
-            <HeaderIndex>
-                <ImgHeader src={"./img/img_logo1.jpg"} ></ImgHeader>
+            <HeaderIndex className="d-flex justify-content-center align-items-center    ">
+                <ImgHeader src={imagemLogo} />
+                <div className="d-flex justify-content-center position-absolute w-75">
+                    <div className="d-flex w-75 p-5 flex-column text-white text-center" style={{backgroundColor: "rgba(0,0,0,0.5)"}}>
+                        <h1 className="text-uppercase " style={{fontFamily: "Algerian", fontSize: "2.5rem"}}>
+                            EBUTDER GAMES
+					    </h1>
+                        <p className="" style={{fontSize: "2.2rem",lineHeight: 1.5 }}>
+                            "A melhor solução para quem quer jogar sem gastar."
+					    </p>
+                    </div>
+                </div>
             </HeaderIndex>
         )
     }
